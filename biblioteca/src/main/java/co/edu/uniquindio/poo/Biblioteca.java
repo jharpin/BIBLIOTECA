10a40d85a0a5cc13a38a7663849c51d55946993b
Fray-Gonzalez-UQ <fraye.gonzalezv@uqvirtual.edu.co>
2024-09-28 16:09:01 -0500

    Documentación completada
    Documentación base de biblioteca  terminada, faltan metodos y mas

--- biblioteca/src/main/java/co/edu/uniquindio/poo/Biblioteca.java
+++ biblioteca/src/main/java/co/edu/uniquindio/poo/Biblioteca.java
@@ -2,7 +2,10 @@ package co.edu.uniquindio.poo;
 
 import java.util.Collection;
 import java.util.LinkedList;
-
+/*
+ *  Clase de una biblioteca, Almacena información sobre
+ * bibliotecarios, préstamos, libros, estudiantes y el dinero recaudado.
+ */  
 public class Biblioteca {
     private String nombre;
     private double totalDineroRecaudo;
@@ -10,7 +13,9 @@ public class Biblioteca {
     private Collection<Prestamo> prestamos;
     private Collection<Libro> libros;
     private Collection<Estudiante> estudiantes;
-
+ /*
+ *  Metodo constructor de la clase biblioteca 
+ */   
     public Biblioteca(String nombre, double totalDineroRecaudo) {
         this.nombre = nombre;
         this.totalDineroRecaudo = totalDineroRecaudo;
@@ -79,15 +84,21 @@ public class Biblioteca {
     public void setLibros(Collection<Libro> libros) {
         this.libros = libros;
     }
-
+/*
+ * Metodo para tomar la lista de estudiantes
+ */
     public Collection<Estudiante> getEstudiantes() {
         return estudiantes;
     }
-
+/*
+ * Metodo para modificar la lista de estudiantes
+ */
     public void setEstudiantes(Collection<Estudiante> estudiantes) {
         this.estudiantes = estudiantes;
     }
-
+/*
+ * Metodo para representar los datos en forma de cadena de la biblioteca
+ */
     @Override
     public String toString() {
         return "Biblioteca [nombre=" + nombre + ", totalDineroRecaudo=" + totalDineroRecaudo + ", bibliotecarios="
