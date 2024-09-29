3898a06a7f691a6ec4ce454b03311cdc9006a3a3
Fray-Gonzalez-UQ <fraye.gonzalezv@uqvirtual.edu.co>
2024-09-29 16:51:57 -0500

    METODOS AGREGADOS BIBLIOTECA
    Clase biblioteca con metodos nuevos y documentados, revisar

--- biblioteca/src/main/java/co/edu/uniquindio/poo/Biblioteca.java
+++ biblioteca/src/main/java/co/edu/uniquindio/poo/Biblioteca.java
@@ -107,5 +107,47 @@ public class Biblioteca {
                 + bibliotecarios + ", prestamos=" + prestamos + ", libros=" + libros + ", estudiantes=" + estudiantes
                 + "]";
     }
+/*
+ * Metodo para agregar un bibliotecario a la lista de bibliotecarios
+ */
+    public void agregarBibliotecario (Bibliotecario bibliotecario) {
+        bibliotecarios.add(bibliotecario);
+    }
 
+/*
+ * Metodo para agregar estudiantes a la lista de estudiantes
+ */
+    public void agregarEstudiante (Estudiante estudiante) {
+        estudiantes.add(estudiante);
+    }
+
+/*
+ * Metodo para agregar libros a la lista de libros
+ */
+    public void agregarLibro (Libro libro) {
+        libros.add(libro);
+    }
+/*
+ * Metodo para crear un prestamo y añadirlo a la lista de prestamos
+ */
+    public void crearPrestamo (Prestamo prestamo) {
+        prestamos.add(prestamo);
+    }
+/*
+ * Metodo para consultar un libro en la lista de libros
+ * El libro se busca mediante su CODIGO
+ */
+    public Libro buscarLibro (String codigo) {
+        for (Libro libro : libros) {
+            if (libro.getCodigo().equals(codigo)) {
+                return libro;
+            }
+        }
+        return null;
+ // no se si deberia poner un elif para pone un mensaje diciendo que el libro no existe   
+    }
+/*
+ * Metodo para consultar los prestamos que estan involucrados por libro
+ */
+   
 }
